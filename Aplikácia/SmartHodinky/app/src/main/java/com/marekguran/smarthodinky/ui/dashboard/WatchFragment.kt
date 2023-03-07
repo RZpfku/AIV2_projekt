a19bc6ad6b953b9492c19a6015ef786d80eb8327
Marek Guráň <skemgi2@gmail.com>
2023-03-07 10:17:07 +0100

    Základná kostra aplikácie

---
+++ Aplikácia/SmartHodinky/app/src/main/java/com/marekguran/smarthodinky/ui/dashboard/WatchFragment.kt
@@ -0,0 +1,32 @@
+package com.marekguran.smarthodinky.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marekguran.smarthodinky.databinding.FragmentWatchBinding
+
+class WatchFragment : Fragment() {
+
+    private var _binding: FragmentWatchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWatchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ Aplikácia/SmartHodinky/app/src/main/java/com/marekguran/smarthodinky/ui/notifications/SettingsFragment.kt
@@ -0,0 +1,33 @@
+package com.marekguran.smarthodinky.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marekguran.smarthodinky.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
