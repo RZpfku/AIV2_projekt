cff3e0d1e7fa27d4458e882c6f31a11cbb5b22f5
Marek Guráň <skemgi2@gmail.com>
2023-03-24 14:58:13 +0100

    Aplikácia teraz farebne ukazuje ukazovatele

--- Aplikácia/Teplomer/app/src/main/java/com/marekguran/esp32teplomer/HomeFragment.kt
+++ Aplikácia/Teplomer/app/src/main/java/com/marekguran/esp32teplomer/HomeFragment.kt
@@ -50,6 +50,34 @@ class HomeFragment : Fragment() {
                             override fun onDataChange(dataSnapshot: DataSnapshot) {
                                 val teplota = dataSnapshot.value as? String ?: ""
                                 binding?.teplotaValue?.text = teplota
+
+                                // Extract the numerical value from the temperature string
+                                val teplotaValue = teplota.substringBefore("℃").toFloatOrNull() ?: 0f
+
+                                if (teplotaValue < 14f) {
+                                    // Set the progress ring color to "zima"
+                                    val colorResId = R.color.zima
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.temperatureDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
+                                if (teplotaValue >= 14f) {
+                                    // Set the progress ring color to "chladno"
+                                    val colorResId = R.color.chladno
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.temperatureDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
+                                if (teplotaValue >= 21f) {
+                                    // Set the progress ring color to "normalna"
+                                    val colorResId = R.color.normalna
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.temperatureDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
+                                if (teplotaValue >= 27f) {
+                                    // Set the progress ring color to "teplo"
+                                    val colorResId = R.color.teplo
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.temperatureDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
                             }
 
                             override fun onCancelled(error: DatabaseError) {
@@ -63,6 +91,26 @@ class HomeFragment : Fragment() {
                             override fun onDataChange(dataSnapshot: DataSnapshot) {
                                 val vlhkost = dataSnapshot.value as? String ?: ""
                                 binding?.vlhkostValue?.text = vlhkost
+
+
+                                // Extract the numerical value from the temperature string
+                                val vlhkostValue = vlhkost.substringBefore("%").toFloatOrNull() ?: 0f
+
+                                if (vlhkostValue < 30f) {
+                                    val colorResId = R.color.vlhkost_mala
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.humidityDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
+                                if (vlhkostValue >= 30f) {
+                                    val colorResId = R.color.vlhkost_optimalna
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.humidityDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
+                                if (vlhkostValue >= 60f) {
+                                    val colorResId = R.color.vlhkost_velka
+                                    ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                    binding?.humidityDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                }
                             }
 
                             override fun onCancelled(error: DatabaseError) {
@@ -76,7 +124,34 @@ class HomeFragment : Fragment() {
                             override fun onDataChange(dataSnapshot: DataSnapshot) {
                                 val vzduch = dataSnapshot.value as? String ?: ""
                                 binding?.vzduchValue?.text = vzduch
+
+                                val colorResId: Int
+                                val airQuality: String
+
+                                when (vzduch) {
+                                    "Dobrá" -> {
+                                        colorResId = R.color.vzduch_dobry
+                                        airQuality = "Dobrá"
+                                    }
+                                    "Stredná" -> {
+                                        colorResId = R.color.vzduch_stredny
+                                        airQuality = "Stredná"
+                                    }
+                                    "Zlá" -> {
+                                        colorResId = R.color.vzduch_zly
+                                        airQuality = "Zlá"
+                                    }
+                                    else -> {
+                                        colorResId = R.color.purple_200 // set default color if value is not recognized
+                                        airQuality = ""
+                                    }
+                                }
+
+                                ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring)?.setTint(ContextCompat.getColor(requireContext(), colorResId))
+                                binding?.airDial?.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.progress_ring))
+                                binding?.vzduchValue?.text = airQuality
                             }
+
 
                             override fun onCancelled(error: DatabaseError) {
                                 // Handle any errors that may occur while retrieving the data
