596b7a9bb1552b9bff8757b29f934ddd5181254f
Marek Guráň <skemgi2@gmail.com>
2023-03-25 15:36:11 +0100

    Aplikácia je teraz optimalizovaná na šírku

--- Aplikácia/Teplomer/app/src/main/java/com/marekguran/esp32teplomer/MainActivity.kt
+++ Aplikácia/Teplomer/app/src/main/java/com/marekguran/esp32teplomer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.marekguran.esp32teplomer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigationrail.NavigationRailView
 import com.marekguran.esp32teplomer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +21,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        // Get the current device orientation
+        val currentOrientation = resources.configuration.orientation
 
-        val navView: BottomNavigationView = binding.navView
+        // Check if the device is in landscape orientation
+        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Use the NavigationRailView instead of the BottomNavigationView
+            val navView: NavigationRailView = binding.navView as NavigationRailView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_about))
-        // setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+            navView.setupWithNavController(navController)
+        } else {
+            // Use the BottomNavigationView for portrait orientation
+            val navView: BottomNavigationView = binding.navView as BottomNavigationView
+
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_about))
+
+            navView.setupWithNavController(navController)
+        }
     }
+
 }
